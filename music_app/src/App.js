813dbcd0b1edbb660f736af53e20da1b1631f599
G <53386620+Gnola@users.noreply.github.com>
2019-12-13 22:51:09 -0800

    changed development to production

--- music_app/src/App.js
+++ music_app/src/App.js
@@ -5,7 +5,7 @@ import Main from './components/Main'
 
 let baseUrl = '';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   baseUrl = 'http://localhost:8888'
 } else {
   baseUrl = 'https://mighty-earth-87374.herokuapp.com/'
