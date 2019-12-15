b5f9756e9170e7a0020efa421a9a26cb4d34118d
Richard Brown <richardbrown@Richard.local>
2019-12-14 16:48:41 -0800

    added header for CORS

--- api/models/music.php
+++ api/models/music.php
@@ -1,5 +1,8 @@
 <?php
 // $dbconn = pg_connect("host=localhost dbname=music");
+header("Access-Control-Allow-Origin:*");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Headers: Content-Type");
 $dbconn = pg_connect(getenv("DATABASE_URL"));
 
 class Song {
