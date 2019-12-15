651aa935ca2d495b439bf14b6a9fb8ba9ea6d7d5
Richard Brown <richardbrown@Richard.local>
2019-12-14 17:09:12 -0800

    CORS

--- api/models/music.php
+++ api/models/music.php
@@ -1,8 +1,5 @@
 <?php
 // $dbconn = pg_connect("host=localhost dbname=music");
-header("Access-Control-Allow-Origin:*");
-header("Content-Type: application/json");
-header("Access-Control-Allow-Headers: Content-Type");
 $dbconn = pg_connect(getenv("DATABASE_URL"));
 
 class Song {
